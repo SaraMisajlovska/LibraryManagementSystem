53aa033741b15cac0c5ff810adbf316558327449
SaraMisajlovska <smisajlovska@gmail.com>
2023-04-04 19:04:35 +0200

    added ddl and data generation script

---
+++ DataGeneration.sql
@@ -0,0 +1,9 @@
+INSERT INTO membership (price, package)
+VALUES (150.0, 'STUDENT'),
+       (200.0, 'STANDARD'),
+       (300.0, 'SENIOR');
+
+INSERT INTO membership (price, package)
+VALUES (150.0, 'STUDENT'),
+       (200.0, 'STANDARD'),
+       (300.0, 'SENIOR');
