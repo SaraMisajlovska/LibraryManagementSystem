69a36cf9e681d3d18b1d361993c1c4aed1f56335
SaraMisajlovska <smisajlovska@gmail.com>
2023-04-04 23:14:59 +0200

    added default value for the card_number and improved the generation query for patron

--- DataGeneration.sql
+++ DataGeneration.sql
@@ -1,35 +1,25 @@
+-- insert statement for memberships
 INSERT INTO membership (price, package)
 VALUES (150.0, 'STUDENT'),
        (200.0, 'STANDARD'),
        (300.0, 'SENIOR');
 
-CREATE SEQUENCE patron_sequence
+--sequence that creates a patron card number and make it default for the patron's card_number
+CREATE SEQUENCE card_number_seq
     increment 3
-    start 100000;
+    start 100005;
+ALTER TABLE patron ALTER COLUMN card_number SET DEFAULT nextval('card_number_seq');
 
-SELECT nextval('patron_sequence');
-
--- loop for creating a patron from lib user, remove loops
-do
-$$
-    declare
-        tmp_user record;
-        card_num integer;
-        mem_id integer;
-    begin
-        for tmp_user in select email, user_password, first_name, last_name, date_of_birth, address, phone_number
-                        from library_user
-            loop
-                card_num = (select nextval('patron_sequence'));
-                mem_id = (floor(random() * (6-4+1) + 4)::int);
-                INSERT
-                INTO patron ( email, user_password, first_name, last_name, date_of_birth, address, phone_number,
-                             card_number,
-                             membership_id)
-
-                VALUES (tmp_user.email, tmp_user.user_password, tmp_user.first_name, tmp_user.last_name,
-                        tmp_user.date_of_birth, tmp_user.address, tmp_user.phone_number, card_num, mem_id);
-            end loop;
-    end;
-$$;
+-- insert statement that creates a patron from library_user
+INSERT INTO patron (email, user_password, first_name, last_name, date_of_birth, address, phone_number,
+                    membership_id)
+SELECT u.email,
+       u.user_password,
+       u.first_name,
+       u.last_name,
+       u.date_of_birth,
+       u.address,
+       u.phone_number,
+       (floor(random() * (6 - 4 + 1) + 4)::int)
+FROM library_user u;
 
