3a460c7d6aafcb9629f5a62728731b794d903473
Angela Milosheska <78094045+thedipsy@users.noreply.github.com>
2023-05-05 18:25:48 +0200

    Update views.sql

--- views.sql
+++ views.sql
@@ -18,7 +18,7 @@ SELECT p.first_name                    AS patron_first_name,
        c.category_name                 AS book_category,
        r.reservation_date              AS reservation_date,
        r.reservation_status            AS reservation_status
-FROM book_reservation r
+FROM book_reservation_new r
          JOIN book_copy bc ON bc.id = r.book_copy_id
          JOIN book b ON b.id = bc.book_id
          JOIN book_category bcg ON bcg.book_id = b.id
@@ -38,7 +38,7 @@ SELECT p.first_name,
        string_agg(a.author_name, ', ')   AS book_author,
        string_agg(c.category_name, ', ') AS book_category,
        br.reservation_date
-FROM book_reservation br
+FROM book_reservation_new br
          JOIN patron p ON p.id = br.user_id
          JOIN book_copy bc ON bc.id = br.book_copy_id
          JOIN book b ON b.id = bc.book_id
@@ -84,8 +84,8 @@ SELECT u.first_name,
            END
 FROM library_user u
          LEFT JOIN payment pt ON u.id = pt.user_id
-          JOIN patron p ON u.id = p.id
-          JOIN membership m ON p.membership_id = m.id
+         JOIN patron p ON u.id = p.id
+         JOIN membership m ON p.membership_id = m.id
 GROUP BY u.id, u.first_name, u.last_name, u.date_of_birth, u.address, u.phone_number, p.card_number, m.package;
 
 
@@ -99,8 +99,8 @@ GROUP BY e.id, e.event_name, e.description, e.event_datetime;
 
 
 --view 12 show reading list
-CREATE  VIEW view_reading_list AS
-SELECT patron.card_number, book.title, book.publisher_id, book.publication_date, book.summary
+CREATE VIEW view_reading_list AS
+SELECT patron.card_number, book.title, book.publisher_id, book.publication_date
 FROM reading_list
          JOIN patron ON reading_list.user_id = patron.id
          JOIN book ON reading_list.book_id = book.id;
@@ -123,36 +123,8 @@ FROM library_user u
 --view 8 check all users
 CREATE VIEW librarian_users AS
 SELECT id, first_name, last_name, email, phone_number
-FROM library_user;
+FROM patron;
 
-
---view 5 Check book details, author, publisher, rating, review, and availability for book (user view)
-CREATE VIEW book_details AS
-SELECT b.id              AS book_id,
-       b.title           AS book_title,
-       p.publisher_name,
-       a.author_name,
-       bc.edition,
-       bc.book_format,
-       bc.location_id,
-       br.rating,
-       rv.review,
-       CASE WHEN bb.id IS NULL THEN 'Available' ELSE 'Borrowed' END AS availability
-FROM book AS b
-         JOIN publisher AS p ON b.publisher_id = p.id
-         JOIN book_copy AS bc ON b.id = bc.book_id
-         JOIN book_author AS ba ON b.id = ba.book_id
-         JOIN author AS a ON ba.author_id = a.id
-         LEFT JOIN book_rating AS br ON b.id = br.book_id
-         LEFT JOIN book_review AS rv ON b.id = rv.book_id
-         LEFT JOIN book_borrow AS bb ON bc.id = bb.book_copy_id AND bb.book_return IS NULL;
-
---for testing
-SELECT *
-FROM book_details;
-SELECT *
-FROM book_details
-WHERE book_title = 'The Catcher in the Rye';
 
 --view 6  Check book availability, locations, and status of all copies – if available show location if not show user that has it (librarian)
 --The view retrieves the book title, edition, format, status, and location of each book copy.
@@ -164,7 +136,7 @@ SELECT book.title            AS book_title,
        book_copy.edition     AS edition,
        book_copy.book_format AS format,
        CASE
-           WHEN book_copy.id IN (SELECT book_copy_id FROM book_borrow WHERE book_return is NULL) THEN 'BORROWED'
+           WHEN book_copy.id IN (SELECT book_copy_id FROM book_borrow WHERE book_return IS NULL) THEN 'BORROWED'
            ELSE 'AVAILABLE'
            END               AS status,
        CASE
@@ -180,10 +152,9 @@ SELECT book.title            AS book_title,
 FROM book_copy
          INNER JOIN book ON book_copy.book_id = book.id;
 
-
 --view 11 show history of borrow
 CREATE VIEW borrowed_books_history AS
-SELECT b.id                               AS borrow_id,
+SELECT bb.id                              AS borrow_id,
        u.first_name || ' ' || u.last_name AS borrower_name,
        p.card_number,
        b.title                            AS book_title,
@@ -192,7 +163,7 @@ SELECT b.id                               AS borrow_id,
        bb.book_checkout                   AS checkout_date,
        bb.book_return                     AS return_date,
        bb.damage                          AS damage_description,
-       c.category_name                    AS category
+       string_agg(c.category_name, ', ')  AS book_category
 FROM book_borrow bb
          JOIN patron p ON bb.user_id = p.id
          JOIN library_user u ON p.id = u.id
@@ -202,6 +173,6 @@ FROM book_borrow bb
          JOIN author a ON ba.author_id = a.id
          JOIN book_location bcl ON bc.location_id = bcl.id
          JOIN book_category bca ON b.id = bca.book_id
-         JOIN category c ON bca.category_id = c.id;
-
-
+         JOIN category c ON bca.category_id = c.id
+group by bb.book_return, a.author_name, bc.book_format, bb.book_checkout, bb.damage, b.title, p.card_number,
+         u.first_name, u.last_name, bb.id;
